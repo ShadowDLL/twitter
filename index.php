6f485e7ff71397206bd02395b9f5a78e1805026d
ORLANDO N <ORLANDO N@Orlando>
2016-12-15 19:41:20 -0200

    Esse é meu terceiro commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Este é o site do twitter
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
